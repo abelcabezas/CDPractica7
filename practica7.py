52dd4df2a1027092859a751be878bb42cfa38fb9
acabezas <abel.cabezas.alonso@cern.ch>
2020-12-15 21:31:54 +0100

    add app 2

--- practica7.py
+++ practica7.py
@@ -48,12 +48,14 @@ def get_medium_distance():
     car_accidents = sc.textFile(car_accidents_file)
     media = car_accidents.map(lambda s: s.split(",")[1])
     count = media.map(lambda value: ("media", value )).reduceByKey(add)
-    side_columns = count.map(
+    media_columns = count.map(
         lambda p: Row(media=p[0], valor=float(p[1])))
+    sqlContext = SQLContext(sc)
+    schemaMedia = sqlContext.createDataFrame(media_columns)
+    sqlContext.select("media").show()
 
     numero_de_registros = car_accidents.count()
     print("Suma:"+str(numero_de_registros))
-
     end = timer()
     elapsed=end - start
     print("Tiempo total: "+str(elapsed)+" segundos")
