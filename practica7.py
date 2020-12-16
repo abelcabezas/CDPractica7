315ff61b0ad58b43425402dfbb27a5a8c5bafcad
acabezas <abel.cabezas.alonso@cern.ch>
2020-12-16 20:36:58 +0100

    sorted

--- practica7.py
+++ practica7.py
@@ -50,7 +50,7 @@ def get_medium_distance():
     car_accidents = sc.textFile(car_accidents_file)
     list = car_accidents.map(lambda s: s.split(",")[1]).collect()
     media = sc.parallelize(list).mean
-    print("Tipo de distancia media" + str(media))
+    print("Tipo de distancia media" + str(type(media)))
     numero_de_registros = car_accidents.count()
     print("Numero de registros:" + str(numero_de_registros))
     end = timer()
