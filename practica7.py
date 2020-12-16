0df16bffffc2be3070c474daee123795b29874b5
acabezas <abel.cabezas.alonso@cern.ch>
2020-12-16 21:52:26 +0100

    to data frame

--- practica7.py
+++ practica7.py
@@ -54,6 +54,7 @@ def get_medium_distance():
     #suma = rdd.values().sum()
     df = count.toDF()
     df.printSchema()
+    df.groupby("Media").agg({'Media': 'mean'}).show()
     #print("Tipo de suma:"+str(type(rdd)))
 
     #df_basket1.groupby('Item_group').agg({'Media': 'mean'}).show()
