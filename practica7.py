a83fea01d5cb3062c2f365d737c1ff7f900d77a7
acabezas <abel.cabezas.alonso@cern.ch>
2020-12-16 21:38:19 +0100

    sorted

--- practica7.py
+++ practica7.py
@@ -51,7 +51,7 @@ def get_medium_distance():
     distances = car_accidents.map(lambda s: s.split(",")[1])
     count = distances.map(lambda distance: ("Media", distance)).collect()
     rdd = sc.parallelize(count)
-    suma = rdd.map(lambda x: x[1]).sum()
+    suma = rdd.values().sum()
     print("Tipo de suma:"+str(type(suma)))
     '''
     list = car_accidents.map(lambda s: s.split(",")[1]).collect()
