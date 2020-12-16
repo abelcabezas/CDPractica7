8a1aac508218e3f36cb68b9baeec514254bf9d5e
acabezas <abel.cabezas.alonso@cern.ch>
2020-12-16 18:42:01 +0100

    add app 5

--- practica7.py
+++ practica7.py
@@ -48,8 +48,8 @@ def get_medium_distance():
     car_accidents_file = "/user/practica6/preprocessed_car_accidents.csv"
     car_accidents = sc.textFile(car_accidents_file)
     media = car_accidents.map(lambda s: s.split(",")[1])
-    count = media.map(lambda value: ("Media", value)).reduceByKey(add).collect()
-    print("Tipo de distancia media"+str(count[0]))
+    count = media.map(lambda value: ("Media", value)).mean()
+    print("Tipo de distancia media"+str(count))
     numero_de_registros = car_accidents.count()
     print("Numero de registros:"+str(numero_de_registros))
     end = timer()
